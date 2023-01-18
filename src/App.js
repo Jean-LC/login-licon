e20d277958e076d533c0a8a6d8afc56966addd34
Jean-LC <jeanettelc@outlook.com>
2023-01-18 17:38:43 -0600

    starts login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='grid-template'>
+
+      <img src={logo} alt='logo'></img>
+      <h3 className='login'>LOGIN</h3>
+      <h3 className='register'>REGISTER</h3>
+      <section className='input-register'>
+        <form>
+          <input className='user'
+            placeholder='Username'
+            type='text'
+            onChange={(e) => setUser(e.target.value)}>
+          </input>
+          <input className='password'
+            placeholder='Password'
+            type='password'>
+          </input>
+          <button className='button-login' type='submit' onClick={(e) => {
+            e.preventDefault();
+            console.log(user)
+          }}>ACCEPT</button>
+        </form>
+      </section>
+
+    </section>
   );
 }
 
